165f9da8059a3205faa7602eceadbbbb4a263ff1
SOGAZG1598 <132963603+SOGAZG1598@users.noreply.github.com>
2024-08-14 11:33:28 -0600

    Update news.js

--- src/data/news.js
+++ src/data/news.js
@@ -1,5 +1,6 @@
 const news = [
     { title: 'Bienvenidos a la Página Oficial de TheMarioDimension!!', content: 'Bienvenidos a la sección de noticias de la página oficial. Aquí encontraran la información más importante de el canal. \nHablando ahora de la página, despues de varios meses de trabajo, esto fue lo que logramos. Yo creo que la página en el estado óptimo para poder lanzarse. Tiene 7 pestañas y más de 70 subpáginas donde cada una se ajusta al tamaño de tu dispositivo. Los invito a probarla y mandarme un correo si encuantran algún bug. \n Pues bueno ojalá les guste la página. ' },
+    { title: 'Hora de arreglar la página', content: 'Hola a todos. Muchas cosas pasaron en este último mes, pero quiero informarles que esta página sigue en pie. \nAhora estoy trabajando en relanzar los videos antiguos y poner los links nuevos en la página. Esto se hará periódicamente, pero el link de mi canal ya debería funcionar. Por lo pronto es todo. Gracias a todos por usar esta página.' }
 ];
 
 export default news;
