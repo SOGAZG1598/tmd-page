b62c6ca6f92f25de59709b663b936feaeb272cca
SOGAZG1598 <132963603+SOGAZG1598@users.noreply.github.com>
2024-10-21 17:09:17 -0600

    Update news.js

--- src/data/news.js
+++ src/data/news.js
@@ -3,6 +3,5 @@ const news = [
     { title: 'Hora de arreglar la página', content: 'Hola a todos. Muchas cosas pasaron en este último mes, pero quiero informarles que esta página sigue en pie. \nAhora estoy trabajando en relanzar los videos antiguos y poner los links nuevos en la página. Esto se hará periódicamente, pero el link de mi canal ya debería funcionar. Por lo pronto es todo. Gracias a todos por usar esta página.' },
     { title: 'LA PÁGINA ESTÁ DE VUELTA! Bueno, casi...', content: 'Hola a todos, gracias por visitar la página! Ahora por fin pueden disfrutar de todos los videos de TMD y TMD2 sin ningún problema. Arreglé todos los videos, solo falta agregar algunos que faltan como el teaser del Preguntas Dimensionales #7, el teaser del de 2000 Subs y agregar el nuevo video lanzado. Esto lo haré en las proximas semanas, igual aquí en la página se van a desplegar los ganadores de las estrellas. Nos vemos pronto y disfruten de TMD y TMD2 esperemos sin interrupciones.' }
 ];
-];
 
 export default news;
