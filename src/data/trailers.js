ca746502cc76bda172258ec4eb5496ba40cafb12
SOGAZG1598 <132963603+SOGAZG1598@users.noreply.github.com>
2024-10-21 17:02:30 -0600

    Update trailers.js

--- src/data/trailers.js
+++ src/data/trailers.js
@@ -12,7 +12,7 @@ const Trailers = [
         section: 'Trailers',
         title: 'Dúper Mario Bros - Teaser',
         thumbnailUrl: DMBT1,
-        webUrl: 'https://www.youtube.com/embed/bKQI9IXvD6M', // Add the episode URL
+        webUrl: 'https://www.youtube.com/embed/wQSsQHrIS8k', // Add the episode URL
         backgroundImage: DMBPT1,
         seriesLogo: DMBLogo,
         description: 'Así es, la continuación de Dúper Mario Bros está en desarrollo. Sé que hay muchas preguntas pero paciencia, todo a su tiempo.',
@@ -22,7 +22,7 @@ const Trailers = [
         section: 'Trailers',
         title: 'Dúper Mario Bros - Teaser #2',
         thumbnailUrl: DMBT2,
-        webUrl: 'https://www.youtube.com/embed/Fb5PmM9maJw', // Add the episode URL
+        webUrl: 'https://www.youtube.com/embed/pHAgZtUf5sM', // Add the episode URL
         backgroundImage: DMBPT2,
         seriesLogo: DMBLogo,
         description: 'La aventura continua, y con ella nuevos retos... Lo mostrado en este teaser está sujeto a cambiar y no tepresenta el resultado final (Voces, visuales y música serán mejor)',
@@ -32,7 +32,7 @@ const Trailers = [
         section: 'Trailers',
         title: 'Dúper Mario Bros - Teaser #3',
         thumbnailUrl: DMBT3,
-        webUrl: 'https://www.youtube.com/embed/bgy4rxW8YEA', // Add the episode URL
+        webUrl: 'https://www.youtube.com/embed/GMVIoQlH9Qs', // Add the episode URL
         backgroundImage: DMBPT3,
         seriesLogo: DMBLogo,
         description: 'Mario y los demás se preparan para su siguiente reto. No se pierdan este año LA OTRA PARTE DE Dúper Mario Bros - Episodio 12...',
