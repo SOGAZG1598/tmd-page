b7b510c08dd77b4c9c9af1d1cda3eccc6b5e4be7
SOGAZG1598 <132963603+SOGAZG1598@users.noreply.github.com>
2024-10-21 16:29:22 -0600

    Update Jueguemos.js

--- src/data/Jueguemos.js
+++ src/data/Jueguemos.js
@@ -26,7 +26,7 @@ const Jueguemos = [
     section: 'Jueguemos',
     title: 'Minecraft Supervivencia Parte 1 - Cuanta lana negra!',
     thumbnailUrl: Jueguemos1,
-    webUrl: 'https://www.youtube.com/embed/7xOnUIQmIvM', // Add the episode URL
+    webUrl: 'https://www.youtube.com/embed/xYwsmUjRcq8', // Add the episode URL
     backgroundImage: JAP1,
     seriesLogo: JALogo,
     description: 'Iniciamos con esta madre!',
@@ -36,7 +36,7 @@ const Jueguemos = [
     section: 'Jueguemos',
     title: 'Minecraft Supervivencia Parte 2 - Matando Cerditos Inocentes :D',
     thumbnailUrl: Jueguemos2,
-    webUrl: 'https://www.youtube.com/embed/B-AuqkSJUxQ', // Add the episode URL
+    webUrl: 'https://www.youtube.com/embed/KFeBUIpEnM8', // Add the episode URL
     backgroundImage: JAP2,
     seriesLogo: JALogo,
     description: 'Seguimos con esta madre!',
@@ -46,7 +46,7 @@ const Jueguemos = [
     section: 'Jueguemos',
     title: 'Minecraft Supervivecia Parte 3 - Publicidad Barata',
     thumbnailUrl: Jueguemos3,
-    webUrl: 'https://www.youtube.com/embed/pregQbuk_dY', // Add the episode URL
+    webUrl: 'https://www.youtube.com/embed/zoPQ_9GYh2g', // Add the episode URL
     backgroundImage: JAP3,
     seriesLogo: JALogo,
     description: 'Entren al server de Endo : endo.servegame.com Y jueguen conmigo, mi nombre de usuario es Rogumer.',
@@ -56,7 +56,7 @@ const Jueguemos = [
     section: 'Jueguemos',
     title: 'Minecraft Supervivencia Parte 4 - Nos Alejamos de Casa',
     thumbnailUrl: Jueguemos4,
-    webUrl: 'https://www.youtube.com/embed/AbPugw73XRY', // Add the episode URL
+    webUrl: 'https://www.youtube.com/embed/RjX8sUv2A3g', // Add the episode URL
     backgroundImage: JAP4,
     seriesLogo: JALogo,
     description: 'Esta madre se empieza a poner interesante ahora que porfin nos alejamos de casa!',
@@ -66,7 +66,7 @@ const Jueguemos = [
     section: 'Jueguemos',
     title: 'Minecraft Supervivencia Parte 5 - I Am Error...',
     thumbnailUrl: Jueguemos5,
-    webUrl: 'https://www.youtube.com/embed/8fEGsrt8tlo', // Add the episode URL
+    webUrl: 'https://www.youtube.com/embed/2X3lK1SXEzA', // Add the episode URL
     backgroundImage: JAP5,
     seriesLogo: JALogo,
     description: 'We continue with this shit!',
@@ -76,7 +76,7 @@ const Jueguemos = [
     section: 'Jueguemos',
     title: 'Minecraft CO-OP Parte 1 - Desmadre al Triple!',
     thumbnailUrl: Jueguemos6,
-    webUrl: 'https://www.youtube.com/embed/gAE_aMo90Zc', // Add the episode URL
+    webUrl: 'https://www.youtube.com/embed/PqVsgP0EQSk', // Add the episode URL
     backgroundImage: JAP6,
     seriesLogo: JALogo,
     description: 'Este video NO es el ganador de la votación de 2000 Subs, el verdadero está tomando un tiempo, y de mientras subo esto, disfrutenlo, que ahora tenemos de compañía a 2 de mis amigos de los que he hablado varias veces, por fin van a conocer a estos 2 desmadrosos XD, y ahora, el desmadre se multiplica por tres!',
@@ -86,7 +86,7 @@ const Jueguemos = [
     section: 'Jueguemos',
     title: 'Minecraft CO-OP Parte 2 - Templo del Desierto',
     thumbnailUrl: Jueguemos7,
-    webUrl: 'https://www.youtube.com/embed/Mqdicpu8Z6I', // Add the episode URL
+    webUrl: 'https://www.youtube.com/embed/ubCAyGBMlb4', // Add the episode URL
     backgroundImage: JAP7,
     seriesLogo: JALogo,
     description: 'Seguimos caminando en busca de comida, topándonos con una casa instantánea!',
@@ -96,7 +96,7 @@ const Jueguemos = [
     section: 'Jueguemos',
     title: 'Minecraft CO-OP Parte 3 - Calabozo del End!',
     thumbnailUrl: Jueguemos8,
-    webUrl: 'https://www.youtube.com/embed/NjlKigMqss8', // Add the episode URL
+    webUrl: 'https://www.youtube.com/embed/NqfanADQJfI', // Add the episode URL
     backgroundImage: JAP8,
     seriesLogo: JALogo,
     description: 'Ahora con unas cuantas mejoras tanto visuales como auditivas, continuamos explorando los alrededores del previamente encontrado Templo del Desierto, solo para encontrarnos con el que sería mi mejor descubrimiento en mi historia jugando Minecraft. OJO : El video NO está laggeado, solo lo están las partes que grabó La Chingada, pues al parecer, la PC de La Chingada tiene una memoria RAM de la chingada. Pun Intended ^___^',
@@ -106,7 +106,7 @@ const Jueguemos = [
     section: 'Jueguemos',
     title: 'Slender Intento 1 - Lag Everywhere',
     thumbnailUrl: Jueguemos9,
-    webUrl: 'https://www.youtube.com/embed/n5zRV0c9t4g', // Add the episode URL
+    webUrl: 'https://www.youtube.com/embed/KRrKQdWPTd0', // Add the episode URL
     backgroundImage: JAP9,
     seriesLogo: JALogo,
     description: 'Iniciamos con una montaña de lag -_-',
@@ -116,7 +116,7 @@ const Jueguemos = [
     section: 'Jueguemos',
     title: 'Slender Intento 2 - ¿Y Bongo Bongo? [Feat. "La Chingada"]',
     thumbnailUrl: Jueguemos10,
-    webUrl: 'https://www.youtube.com/embed/0JNOdHsalZs', // Add the episode URL
+    webUrl: 'https://www.youtube.com/embed/-rraPYYoP9I', // Add the episode URL
     backgroundImage: JAP10,
     seriesLogo: JALogo,
     description: 'Volvemos a intentarlo, ahora con muuucho menos lag, y aparte, estamos acompañados por el señor profesor Dr. Drew Pea Cock! Digo la Chingada.',
