661d28a5f303c776d6b8ad89026d2f532c6766ff
SOGAZG1598 <132963603+SOGAZG1598@users.noreply.github.com>
2024-10-21 16:23:16 -0600

    Update CW.js

--- src/data/CW.js
+++ src/data/CW.js
@@ -10,7 +10,7 @@ const CW = [
     section: 'CW',
     title: 'Navidad 2012',
     thumbnailUrl: CW1,
-    webUrl: 'https://www.youtube.com/embed/ys2mWWBs3J8', // Add the episode URL
+    webUrl: 'https://www.youtube.com/embed/qNTa9S6Ydlc', // Add the episode URL
     backgroundImage: CWP1,
     seriesLogo: CWLogo,
     description: 'Inicio esta nueva mini serie de animaciónes simples para entrenar mi habilidad esta... Tanpoco me pregunten cuando sale el próximo POR FAVOR, aunque estoy pensando en hacer uno de Año Nuevo, no es oficial eh, en fin, Feliz Navidad!',
@@ -20,7 +20,7 @@ const CW = [
     section: 'CW',
     title: 'Año Nuevo',
     thumbnailUrl: CW2,
-    webUrl: 'https://www.youtube.com/embed/n-043mXislc', // Add the episode URL
+    webUrl: 'https://www.youtube.com/embed/nfkOdpxD_ks', // Add the episode URL
     backgroundImage: CWP2,
     seriesLogo: CWLogo,
     description: 'En Navidad y Año Nuevo (sobre todo Año Nuevo) mucha gente acostumbra disparar al cielo como si estuvieran "matando el año" PUES NO LO HAGAN! No sabes a que le caerán tus balas, puedes dañar las pertenencias de otros o hasta quitarle la vida a algúnos. Este episodio está más corto que el anterior pero solo tuve 1 semana para desarrollarlo, ojala me entiendan, Navidad y Año Nuevo son épocas muy pegadas, pero disfrutenlo :)',
