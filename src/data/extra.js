75763f5570139f54660ca96646c22ff231b72497
SOGAZG1598 <132963603+SOGAZG1598@users.noreply.github.com>
2024-10-21 17:01:21 -0600

    Update extra.js

--- src/data/extra.js
+++ src/data/extra.js
@@ -16,7 +16,7 @@ const Extras = [
         section: 'Extra',
         title: '2000 Subs FTW',
         thumbnailUrl: S20001,
-        webUrl: 'https://www.youtube.com/embed/mgcaloP3_m8', // Add the episode URL
+        webUrl: 'https://www.youtube.com/embed/pfGrzfRE4b8', // Add the episode URL
         backgroundImage: S20001P,
         seriesLogo: TMDLOGO,
         description: 'Muchas muchas gracias! Mi meta en un principio era 1000, pero ahora somos el doble! Realmente, muchas gracias! :)',
@@ -26,7 +26,7 @@ const Extras = [
         section: 'Extra',
         title: 'TMD : Draw My Life / Draw My Life in Youtube',
         thumbnailUrl: DMLT,
-        webUrl: 'https://www.youtube.com/embed/mgcaloP3_m8', // Add the episode URL
+        webUrl: 'https://www.youtube.com/embed/ZtmJYsjWgCc', // Add the episode URL
         backgroundImage: DMLP,
         seriesLogo: TMDLOGO,
         description: 'Ustedes lo pidieron, y lo obtuvieron, disculpen la no muy buena calidad, no es fácil grabar un Draw My Life XD. Disfrutenlo, y conozcan un poco más de mi :D. Bueno... por alguna increíblemente extraña razón, muchos creen que yo he insultado al país sudamericano Chile en este video... Y yo así como de O______________________________O WTF!!!! Al decir que no hablaba del país en realidad hablaba del alimento que aquí llamamos "chile", si no lo conoces, es un alimento famoso aquí en México, aún así, no se como rayos se entiende que yo insulté al país, o sea, ¿Dónde diantres se supone que está el insulto? O_O Sería todo lo contrario, en verdad no he visitado esta gran sección del mundo, pero existe el Internet y me he sabido informar sobre su cultura y forma de vivir, que me ha agradado de forma personal influenciando en mis gustos :D',
@@ -36,7 +36,7 @@ const Extras = [
         section: 'Extra',
         title: 'DMB Direct',
         thumbnailUrl: DMBD,
-        webUrl: 'https://www.youtube.com/embed/3gB2SHpHh9U', // Add the episode URL
+        webUrl: 'https://www.youtube.com/embed/UA5Gx1fuJP8', // Add the episode URL
         backgroundImage: DMBDP,
         seriesLogo: TMDLOGO,
         description: 'Hola a todos y bienvenidos al directo de DMB, donde encontrarán lo último de información acerca de esta serie. No se olviden de suscribirse, darle like y más importante, compartir. Ojalá lo disfruten y no se olviden de ver de principio a fin...',
@@ -46,7 +46,7 @@ const Extras = [
         section: 'Extra',
         title: 'Critiquemos a Dúper Mario Bros',
         thumbnailUrl: CADMB,
-        webUrl: 'https://www.youtube.com/embed/VvE-5-9GnKQ', // Add the episode URL
+        webUrl: 'https://www.youtube.com/embed/E-gvbx7nqIc', // Add the episode URL
         backgroundImage: CADMBP,
         seriesLogo: TMDLOGO,
         description: 'Hoy toca hacer review a esta amada serie, y que mejor, tambien veremos lo que opinan ustedes!!',
@@ -56,7 +56,7 @@ const Extras = [
         section: 'Extra',
         title: '2000 Subs FTW - TheMarioDimension2',
         thumbnailUrl: E2000S2,
-        webUrl: 'https://www.youtube.com/embed/D4C8Zs6-Fxo', // Add the episode URL
+        webUrl: 'https://www.youtube.com/embed/j1zk--prg70', // Add the episode URL
         backgroundImage: S2000SP,
         seriesLogo: TMDLOGO,
         description: '2000 Subscriptores se dice facil pero es un logro muy grande para este canal. Gracias y vamos por los 10000 :)',
