ae835f032d65d2fa7cd7347999d461eed528f331
SOGAZG1598 <132963603+SOGAZG1598@users.noreply.github.com>
2024-10-21 16:25:42 -0600

    Update Creepys.js

--- src/data/Creepys.js
+++ src/data/Creepys.js
@@ -16,7 +16,7 @@ const Creepys = [
     section: 'Creepy',
     title: 'Super Mario 64 : El Course 16',
     thumbnailUrl: Creepys1,
-    webUrl: 'https://www.youtube.com/embed/U9lMfadqhGo', // Add the episode URL
+    webUrl: 'https://www.youtube.com/embed/F8MkxRpT3go', // Add the episode URL
     backgroundImage: CreepyP1,
     seriesLogo: CreepyLogo,
     description: 'Mi primer Creepypasta al igual que mi primer video loquendo, no sean duros conmigo, que es mi primera ves haciendo ambas cosas asi que tengo derecho a equivocarme ;)',
@@ -26,7 +26,7 @@ const Creepys = [
     section: 'Creepy',
     title: "Zelda Majora's Mask : La Máscara de la Condena",
     thumbnailUrl: Creepys2,
-    webUrl: 'https://www.youtube.com/embed/xNL6MFuyB5k', // Add the episode URL
+    webUrl: 'https://www.youtube.com/embed/I3Mylg1dvSk', // Add the episode URL
     backgroundImage: CreepyP2,
     seriesLogo: CreepyLogo,
     description: 'Si si, se que está muy largo, es que mi imaginación fue más allá de lo que creí que podría ir cuando hice éste Creepypasta. Recuerden no ir al cementerio de Ikana en el cuarto día...',
@@ -36,7 +36,7 @@ const Creepys = [
     section: 'Creepy',
     title: 'El Despertador',
     thumbnailUrl: Creepys3,
-    webUrl: 'https://www.youtube.com/embed/BWcKaqdVc1k', // Add the episode URL
+    webUrl: 'https://www.youtube.com/embed/NVIEBwW07mY', // Add the episode URL
     backgroundImage: CreepyP3,
     seriesLogo: CreepyLogo,
     description: 'Este TANPOCO es el video ganador de la votación de 2000 Subs, de nuevo, el verdadero ganador está demorando, y de mientras este XD. Bueno, esta historia, la escribí NO con el propósito de cagarte muuuucho de miedo, sino con el de hacer una más extensa, compleja, y completa que mis 2 Creepypastas anteriores, ojala les guste, y por favor pónganle atención :) Pero parece que si los cagó de miedo..... ._.',
@@ -46,7 +46,7 @@ const Creepys = [
     section: 'Creepy',
     title: 'Toc Toc',
     thumbnailUrl: Creepys4,
-    webUrl: 'https://www.youtube.com/embed/AJB2hy8WIgA', // Add the episode URL
+    webUrl: 'https://www.youtube.com/embed/u-XjKO7y3Y', // Add the episode URL
     backgroundImage: CreepyP4,
     seriesLogo: CreepyLogo,
     description: 'Feliz Halloween :) Bueno, este es mi primer Creepypasta EN TERCERA PERSONA, oajala les guste :D. Y cuidado al abrir la puerta, nunca sabes si en verdad se trata de un niño en busca de dulces...',
@@ -56,7 +56,7 @@ const Creepys = [
         section: 'Creepy',
         title: 'Creepypasta : Mira atrás de tí X Marlo [Loquendo]',
         thumbnailUrl: Creepys5,
-        webUrl: 'https://www.youtube.com/embed/4qA-7Z3pEvQ', // Add the episode URL
+        webUrl: 'https://www.youtube.com/embed/jgJuPzYmv60', // Add the episode URL
         backgroundImage: CreepyP5,
         seriesLogo: CreepyLogo,
         description: 'Bienvenidos al especial de Halloween!! Aquí se mostrarán 2 Creepypastas hechos por la comunidad: Mira atrás de tí por Juan Andrés Briceño:  juanandr3sbricen0  / Marlo por Viche Mimi: https://www.facebook.com/profile.php?...',
