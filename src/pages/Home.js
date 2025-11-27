6e8e408210599d27ab76164d89c4127f4b998fda
Sebastián Ogaz <132963603+SOGAZG1598@users.noreply.github.com>
2025-11-26 20:47:18 -0600

    ads

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,12 +6,17 @@ import './Scroll.css'
 
 const App = () => {
     return (
+        <html>
+            <head>
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3081463517624663"
+     crossOrigin="anonymous"></script>
+            </head>
         <div className="app">
-            
-            <Navbar /> {/* Include your Navbar component */}
+            <Navbar />
             <Welcome />
             <Footer />
         </div>
+        </html>
     );
 };
 
