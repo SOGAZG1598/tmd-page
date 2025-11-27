5005fc8c35a92e1eff640803a724cb114fc3a49b
Sebastián Ogaz <132963603+SOGAZG1598@users.noreply.github.com>
2025-11-26 20:51:25 -0600

    Update SocialMedia.js

--- src/pages/SocialMedia.js
+++ src/pages/SocialMedia.js
@@ -8,6 +8,8 @@ import SocialMediat from '../components/Social Media/SocialMediat';
 const SocialMedia = () => {
     return(
         <div>
+        <head><script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3081463517624663"
+     crossOrigin="anonymous"></script></head>
             <Navbar />
             <SocialMediat />
             <Footer />
